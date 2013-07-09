a032245673c0a4f9784363a6be05a769d47c5aae
zeroy <todezou18@163.com>
2013-07-09 15:57:36 +0800

    modified the layout for Activity and initial some variables

--- src/ian/practice/findmaxmumbymutithreads/MaxmumActivity.java
+++ src/ian/practice/findmaxmumbymutithreads/MaxmumActivity.java
@@ -1,17 +1,29 @@
 package ian.practice.findmaxmumbymutithreads;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MaxmumActivity extends Activity {
 
+	int threadNumber;
+	CountDownLatch startSignal = new CountDownLatch(1);
+    CountDownLatch doneSignal;
+    ExecutorService executor;
+    int[] largeArray = new int[10000];
+    int[] result;
+    long startTime;
+    long endTime;
+    long duration;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maxmum);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
