fd26cdb0a299f05b8b26f6dbab597f0fb1ad0614
zeroy <todezou18@163.com>
2013-07-11 15:17:36 +0800

    do some clean!

--- src/ian/practice/findmaxmumbymutithreads/MaxmumActivity.java
+++ src/ian/practice/findmaxmumbymutithreads/MaxmumActivity.java
@@ -1,6 +1,5 @@
 package ian.practice.findmaxmumbymutithreads;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -8,7 +7,6 @@ import java.util.concurrent.Executors;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,96 +16,98 @@ import android.widget.Toast;
 
 public class MaxmumActivity extends Activity {
 
+	int LARGEARRAY_LENGTH = 30000;
 	int threadNumber;
 	CountDownLatch startSignal = new CountDownLatch(1);
-    CountDownLatch doneSignal;
-    ExecutorService executor;
-    int[] largeArray = new int[9];
-    int[] result;
-    int[][] dividedArray;
-    long startTime;
-    long endTime;
-    long duration;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maxmum);
-        final EditText threadNum = (EditText) findViewById(R.id.ThreadNum);
-        final TextView durationMessage = (TextView) findViewById(R.id.DurationInfo);
-        Button startButton = (Button) findViewById(R.id.Start);
-        startButton.setOnClickListener(new OnClickListener(){
+	CountDownLatch doneSignal;
+	ExecutorService executor;
+	int[] largeArray ;
+	int[] result;
+	long startTime;
+	long endTime;
+	long duration;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_maxmum);
+		
+		final EditText threadNum = (EditText) findViewById(R.id.ThreadNum);
+		final TextView durationMessage = (TextView) findViewById(R.id.DurationInfo);
+		largeArray = initialLargeArray(LARGEARRAY_LENGTH);
+		
+		Button startButton = (Button) findViewById(R.id.Start);
+		startButton.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
 				if(threadNum.getText().toString().trim().equals("")){
-					durationMessage.setText("please input a Thread Number !");
 					Toast.makeText(MaxmumActivity.this, "please input a Thread Number", Toast.LENGTH_LONG).show();
 				}else{
 					threadNumber = Integer.parseInt(threadNum.getText().toString());
 					doneSignal = new CountDownLatch(threadNumber);
-	        		executor = Executors.newFixedThreadPool(threadNumber);
-	        		result = new int[threadNumber]; 	 
-	        		
-	        		largeArray = initialLargeArray(150000);
-	        		startTime = System.nanoTime();//System.currentTimeMillis();	   
-	        		int maxmum = FindMaxmumInLargeArray(largeArray);
-	        		endTime = System.nanoTime();//System.currentTimeMillis();
-	        		duration = endTime - startTime;					
+					executor = Executors.newFixedThreadPool(threadNumber);
+					result = new int[threadNumber]; 
+					
+					startTime = System.currentTimeMillis();	 
+					int maxmum = FindMaxmumInLargeArray(largeArray);
+					endTime = System.currentTimeMillis();
+					duration = endTime - startTime;
 					durationMessage.setText("there are " + Integer.toString(threadNumber) + " threads ." 
 											+ "Maxmum is " + Integer.toString(maxmum) 
+											+ " arrayLength is " + Integer.toString(LARGEARRAY_LENGTH) 
 											+ ". duration is:" + Long.toString(duration));
 				}
 				
 			}
-        });
+		});
 
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.maxmum, menu);
-        return true;
-    }
-    
-    private int FindMaxmumInLargeArray(int[] largeArray){
-    	int maxmum = 0 ;
-    	int[] range = new int[2];
-    	int sectionLength = largeArray.length/threadNumber;
-    	  for (int i = 0; i < threadNumber; ++i) // create and start threads
-          {
-    		range[0] = i*sectionLength;
-    	//	System.out.println(Integer.toString(i)+"--range0 = "+ Integer.toString(range[0]));
-    		if(i != threadNumber-1){
-    			range[1] = ((i+1)*sectionLength)-1;
-    		}else{
-    			range[1] = largeArray.length-1;
-    		}
-    	//	System.out.println(Integer.toString(i)+ "---range1 = "+ Integer.toString(range[1]));
-          	executor.execute(new Thread(new Worker(i,range,startSignal,doneSignal)));
-          }
-    	  startSignal.countDown();
-    	  try {
-	    		  doneSignal.await();
-	  		} catch (InterruptedException e) {
-	  			e.printStackTrace();
-	  		} 
-           maxmum = FindMaxmunInArray(result);
-                   	  
-    	return maxmum;
-    }
-    
-    private int[] initialLargeArray(int n){
-    	int[] largeArray = new int[n];
-    	Random rnd = new Random();
-    	
-    	for(int i=0; i < n; i++){
-    		largeArray[i] = rnd.nextInt(1000);
-    	}
-    	
-    	return largeArray;
-    }
-    
+	private int FindMaxmumInLargeArray(int[] largeArray){
+		int maxmum = 0 ;
+		int[] range;
+		int sectionLength = largeArray.length/threadNumber;
+		
+		for (int i = 0; i < threadNumber; ++i) {
+			range = getRange(i,sectionLength);
+			executor.execute(new Thread(new Worker(i,range,startSignal,doneSignal)));
+		}
+		
+		startSignal.countDown();
+		
+		try {
+				doneSignal.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} 
+		 maxmum = FindMaxmunInArray(result);
+
+		return maxmum;
+	}
+
+	private int[] getRange(int i, int sectionLength){
+		int[] range = new int[2];
+
+		range[0] = i*sectionLength;
+		if(i != threadNumber-1){
+			range[1] = ((i+1)*sectionLength)-1;
+		}else{
+			range[1] = largeArray.length-1;
+		}
+		return range;
+	}
+	
+	private int[] initialLargeArray(int n){
+		int[] largeArray = new int[n];
+		Random rnd = new Random();
+		
+		for(int i=0; i < n; i++){
+			largeArray[i] = rnd.nextInt(30001);
+		}
+		
+		return largeArray;
+	}
+	
 	private int FindMaxmunInArray(int[] inputArray){
 		int maxmum = 0;
 	
@@ -119,44 +119,41 @@ public class MaxmumActivity extends Activity {
 		
 	}
 	
-	private int FindMaxmunInSetionOfArray(int[] sectionRange, int[] inputArray)
+	private int FindMaxmunInSetionOfArray(int[] sectionRange)
 	{
 		int maxmum = 0;
 		int rangeLength = sectionRange[1]-sectionRange[0];
 		
 		for(int i=0; i < rangeLength; i++){
-			if(inputArray[i]> maxmum)
-				maxmum = inputArray[i];
-		}
-		
+			if(largeArray[i]> maxmum)
+				maxmum = largeArray[i];
+		}		
 		return maxmum;
 		
 	}
 	
-    
+	
 	class Worker implements Runnable {
-    	private final CountDownLatch doneSignal;
-    	private final CountDownLatch startSignal;
-    	private int[] range;
-    	private int resultIndex = 0;
-    	private int maxValue;
-    	
-    	Worker(int n, int[] range, CountDownLatch startSignal,CountDownLatch doneSignal) {
-    		this.doneSignal = doneSignal;
-    		this.startSignal = startSignal;
-    		this.resultIndex = n;
-    		this.range = range;
-    	}   	
-
-
+		private final CountDownLatch doneSignal;
+		private final CountDownLatch startSignal;
+		private int[] range;
+		private int resultIndex = 0;
+		private int maxValue;
+		
+		Worker(int n, int[] range, CountDownLatch startSignal,CountDownLatch doneSignal) {
+			this.doneSignal = doneSignal;
+			this.startSignal = startSignal;
+			this.resultIndex = n;
+			this.range = range;
+		} 	
+		
 		@Override
 		public void run() {
 			try {
 				startSignal.await();
 			
-				maxValue = FindMaxmunInSetionOfArray(range,largeArray);
+				maxValue = FindMaxmunInSetionOfArray(range);
 				writeMaxmumToResult(maxValue);
-				System.out.println("Thread" + Integer.toString(resultIndex));
 				doneSignal.countDown();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
@@ -164,9 +161,9 @@ public class MaxmumActivity extends Activity {
 			}
 		}
 		
-		private void writeMaxmumToResult(int value){
-			result[resultIndex] = value;
+		private void writeMaxmumToResult(int maxmum){
+			result[resultIndex] = maxmum;
 		}
 		
-    }
+	}
 }
